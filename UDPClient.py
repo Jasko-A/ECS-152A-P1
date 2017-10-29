869120f9bb9d323564bec3bedc2806bf1a3b3d94
mynguyen5194 <mydng@ucdavis.edu>
2017-10-28 18:13:03 -0700

    Changed dayOfWeek to dictionary, while-loop to for-loop. Used datetime library to get utc time (time only gives local time)

--- UDPClient.py
+++ UDPClient.py
@@ -1,49 +1,25 @@
-
 from socket import *
-import time as t
-import string
-import random
-import datetime
+from datetime import *
+from time import *
 
-def getDay(day,newDay):
-	for item in newDay:
-		if(day == item[0]):
-			return item[1]
-		'''
-	for i, item in enumerate(newDay):
-		if item[i][0] == day:
-			print(item[i][1])'''
-
-numPing = 0
 serverName = 'localhost'
 serverPort = 12000
 
-dayOfWeek = [['Monday','M'],['Tuesday','T'],['Wednesday','W'],['Thursday','R'],['Friday','F'], ['Saturday','S'], ['Sunday','U']]
-now = datetime.datetime.now()
+dayOfWeek = {"Mon": 'M', "Tues": 'T', "Wed": 'W', "Thu": 'R', "Fri": 'F', "Sat": 'S', "Sun": 'U'}
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.settimeout(1) 	#sets timeout after message has been sent to the sever
 
-
-
-while numPing < 10:
-	clientSocket.settimeout(1) 	#sets timeout after message has been sent to the sever. Now we
-									#wait for the servers response
-	start_time = t.clock()
-	#day = now.strftime("%A")
-	day = getDay(now.strftime("%A"),dayOfWeek)
-	message = 'Ping ' + str(numPing + 1) + ' ' + now.strftime("%Y-%m-%d ") + day +  now.strftime(" %H:%M ") + 'UTC'
-	#message = raw_input(’Input lowercase sentence:’)
-
+for i in range(1, 11):
+	message = 'Ping ' + str(i) + ' ' + datetime.utcnow().strftime("%Y-%m-%d ") + dayOfWeek[datetime.utcnow().strftime("%a")] +  datetime.utcnow().strftime(" %H:%M %z")
 	print(message)
 	clientSocket.sendto(str.encode(message),(serverName, serverPort))
-
+	start_time = clock()
 	
 	try:
 		modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-		print (bytes.decode(modifiedMessage), "\nRTT:", round(((t.clock() - start_time)*1000),3), "\n")
+		print (bytes.decode(modifiedMessage), "\nRTT:", round(((clock() - start_time)*1000),3), "\n")
 	except timeout:
 		print ("Request timed out\n")
-	numPing = numPing + 1
 
 clientSocket.close()
-
